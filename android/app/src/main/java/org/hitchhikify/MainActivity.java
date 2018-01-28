8c3ece8e1e79e4692e7f099aeafc16abc216aab5
Hlib Babii <hlib.babii@unibz.it>
2018-01-28 10:57:10 +0100

    add draft of profile activity

--- android/app/src/main/java/org/hitchhikify/MainActivity.java
+++ android/app/src/main/java/org/hitchhikify/MainActivity.java
@@ -1,5 +1,6 @@
 package org.hitchhikify;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,13 +9,38 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mStartNewTripButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mStartNewTripButton = (Button) findViewById(R.id.start_new_trip_button);
+        mStartNewTripButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Start new trip", Toast.LENGTH_LONG).show();
+            }
+        });
+        TextView editProfileTextView = (TextView) findViewById(R.id.edit_profile_text_view);
+        editProfileTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Edit profile", Toast.LENGTH_LONG).show();
+            }
+        });
+        CircleImageView circleImageView = (CircleImageView) findViewById(R.id.profile_image);
+        circleImageView.setImageURI(Uri.parse("https://scontent-mxp1-1.xx.fbcdn.net/v/t1.0-1/p160x160/23795485_1795370397162891_851702792683737565_n.jpg?oh=59ee48dac48f21cc37bedacc5423370c&oe=5ADB100A"));
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("Hi there");
         setSupportActionBar(toolbar);
---
+++ android/app/src/main/java/org/hitchhikify/Tile.java
@@ -0,0 +1,46 @@
+package org.hitchhikify;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by hlib on 28.01.18.
+ */
+
+public class Tile extends LinearLayout {
+
+    private Drawable mDrawable;
+    private String mText;
+
+    public Tile(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    private void init(Context context, AttributeSet attrs) {
+        View rootView = inflate(context, R.layout.tile, this);
+        TypedArray typedArray = context.getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.Tile,
+                0, 0);
+
+        try {
+            mDrawable = typedArray.getDrawable(R.styleable.Tile_image_src);
+            mText = typedArray.getString(R.styleable.Tile_text);
+        } finally {
+            typedArray.recycle();
+        }
+
+        TextView textView = (TextView) rootView.findViewById(R.id.tile_text_view);
+        textView.setText(mText);
+        ImageView imageView = (ImageView) rootView.findViewById(R.id.tile_image_view);
+        imageView.setImageDrawable(mDrawable);
+    }
+}
