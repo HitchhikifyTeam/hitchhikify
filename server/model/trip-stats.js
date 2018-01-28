cc5bf34a64267cc49dc07eec21a99c8272bbd140
Hlib Babii <hlib.babii@unibz.it>
2018-01-28 22:47:22 +0100

    add stub api for getting trip stats

---
+++ server/model/trip-stats.js
@@ -0,0 +1,12 @@
+/**
+ * Created by hlib on 28.01.18.
+ */
+
+var tripStats = {
+    "distanceTravelled": 70,
+    "carsTravelled": 7,
+    "tripDuration": 21,
+    "averageSpeed": 34
+};
+
+module.exports = tripStats;
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Hitchhikify' });
+var tripStats = require('../model/trip-stats');
+
+var apiVesrion = "v1.0";
+
+router.get('/api/' + apiVesrion + '/trip-stats/:trip_id', function(req, res, next) {
+  console.log("Getting trip stats for the trip with id= " + req.params.trip_id + " ...");
+  res.json(tripStats);
 });
 
 module.exports = router;
